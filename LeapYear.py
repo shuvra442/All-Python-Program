b09e865ed5cc2ca1dac6853cf04f0d50cce9738b
shuvra442 <shuvrapatra5@gmail.com>
2024-02-07 21:08:55 +0530

    some changes

--- LeapYear.py
+++ LeapYear.py
@@ -1,5 +1,5 @@
 year=int(input("Enter the year :: "))
 if(year%400==0 or year %100 !=0 and year %4 ==0):
-  print("%d is a leap year" %year)
+  print("{0} is a leap year".format(year))
 else:
   print("%d is not a leap year" %year)
